f91a1279ac604d461ae43586108eb83e9dd795a2
rachelBurford <rachel@pilbaratax.com.au>
2021-08-06 18:45:14 +0800

    Save
    Save Progress

--- she_codes_news/users/admin.py
+++ she_codes_news/users/admin.py
@@ -4,12 +4,14 @@ from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+  
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email','username']
+    list_display = ['email','username','image']
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
